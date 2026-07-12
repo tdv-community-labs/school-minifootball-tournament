201f9fc01f6d9d3cf05f67141ff7f9e4acb48150
TheMightyKhan <orxannamazovld@gmail.com>
2026-07-12 12:30:04 +0400

    firebase hazirdir

--- services/firebase-config.js
+++ services/firebase-config.js
@@ -1,13 +1,14 @@
 // Firebase Configuration and Toggle for Real/Mock Database
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAIV2lXg-4ZjZcUW9lp_-QV3hwBLZsmUs",
+  authDomain: "tdv-football.firebaseapp.com",
+  projectId: "tdv-football",
+  storageBucket: "tdv-football.firebasestorage.app",
+  messagingSenderId: "492634553657",
+  appId: "1:492634553657:web:57ad01a633fc4568dd7ce8",
+  measurementId: "G-85XEEMZYGC"
 };
 
 // Set to true when you want to connect to a real Firebase Firestore instance
 // Ensure you have initialized Firebase in your project before setting to true.
-export const useRealFirebase = false;
+export const useRealFirebase = true;
